4e00861c8b77afaf329f91928254ca972d36da83
mangozy <32133971+mangozy@users.noreply.github.com>
2020-07-10 22:51:26 -0400

    update

---
+++ src/data_prep/HippocampusDatasetLoader.py
@@ -0,0 +1,94 @@
+"""
+Module loads the hippocampus dataset into RAM
+"""
+import os
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+from medpy.io import load
+
+from utils.utils import med_reshape
+
+def LoadHippocampusData(root_dir, y_shape, z_shape):
+    '''
+    This function loads our dataset form disk into memory,
+    reshaping output to common size
+
+    Arguments:
+        volume {Numpy array} -- 3D array representing the volume
+
+    Returns:
+        Array of dictionaries with data stored in seg and image fields as 
+        Numpy arrays of shape [AXIAL_WIDTH, Y_SHAPE, Z_SHAPE]
+    '''
+
+    image_dir = os.path.join(root_dir, 'images')
+    label_dir = os.path.join(root_dir, 'labels')
+
+    images = [f for f in listdir(image_dir) if (
+        isfile(join(image_dir, f)) and f[0] != ".")]
+
+    out = []
+    for f in images:
+
+        # We would benefit from mmap load method here if dataset doesn't fit into memory
+        # Images are loaded here using MedPy's load method. We will ignore header 
+        # since we will not use it
+        image, _ = load(os.path.join(image_dir, f))
+        label, _ = load(os.path.join(label_dir, f))
+        
+        # print("original image shape:", image.shape)
+        # print("original label shape:", label.shape)
+
+        # TASK: normalize all images (but not labels) so that values are in [0..1] range
+        # <YOUR CODE GOES HERE>
+        # image = np.clip(image, 0., 1.)
+        image = image.astype(np.single)/np.max(image)
+
+        # We need to reshape data since CNN tensors that represent minibatches
+        # in our case will be stacks of slices and stacks need to be of the same size.
+        # In the inference pathway we will need to crop the output to that
+        # of the input image.
+        # Note that since we feed individual slices to the CNN, we only need to 
+        # extend 2 dimensions out of 3. We choose to extend coronal and sagittal here
+        
+        ################################################# My Comments ###############################################
+        # why don't you just say we need to keep each image with the same size of coronal and sagittal dimensions   #
+        # the raw data has different sizes of coronal and sagittal dimensions                                       #
+        #############################################################################################################
+        
+        # TASK: med_reshape function is not complete. Go and fix it!
+        # image = med_reshape(image, new_shape=(image.shape[0], y_shape, z_shape))
+        # label = med_reshape(label, new_shape=(label.shape[0], y_shape, z_shape)).astype(int)
+        new_shape=(image.shape[0], y_shape, z_shape) # x_shape = axial, y_shape = coronal, z_shape = sagittal
+        image = med_reshape(image, new_shape=new_shape)
+
+        ################################################ My Comments ################################################
+        # I reshaped images here already with the new_shape, which is (num_of_images, 64, 64)                       #
+        # I don't understahd why mentors recommended me to do the following code to reshape an image                #
+        # image = med_reshape(image, new_shape=new_shape) does the same thing already                               #
+        # (image == reshaped_image).all()                                                                           #
+        # Output: True                                                                                              #
+        #############################################################################################################
+        reshaped_image = np.zeros(new_shape)
+        reshaped_image[0:image.shape[0], 0:image.shape[1], 0:image.shape[2]] = image
+        label = med_reshape(label, new_shape=(label.shape[0], y_shape, z_shape)).astype(int)
+        
+        ################################################ My Guess ###################################################
+        # My guess is the images and labels should be converted into (num_of_2D_images, 64, 64)                  #                                                                                            #
+        #############################################################################################################
+        image = med_reshape(image, new_shape=(image.shape[0], y_shape, z_shape))
+        label = med_reshape(label, new_shape=(label.shape[0], y_shape, z_shape)).astype(int)
+        
+        # print("reshaped image shape:", reshaped_image.shape)
+        # print("reshaped label shape:", label.shape)
+        
+        # TASK: Why do we need to cast label to int?
+        # ANSWER: Label is to be used as mask in the images. It is sort of like binary label. It can be "yes" or "nor" area.
+
+        out.append({"image": image, "seg": label, "filename": f})
+
+    # Hippocampus dataset only takes about 300 Mb RAM, so we can afford to keep it all in RAM
+    print(f"Processed {len(out)} files, total {sum([x['image'].shape[0] for x in out])} slices")
+    return np.array(out)
